5b5ee60a4e0e0132af19b79c5b52a14274f7f43a
yyh123 <yunhaoyu@gmail.com>
2020-01-14 18:00:53 +0800

    feat: pending,success,error to suspense

--- src/pokemon-detail.js
+++ src/pokemon-detail.js
@@ -1,5 +1,40 @@
+/* eslint-disable no-labels */
+/* eslint-disable no-label-var */
 import React from 'react'
 
+function suspenseify(promise) {
+  let status = 'pending'
+  let result
+  let suspender = promise.then(
+    response => {
+      status = 'success'
+      result = response
+    },
+    error => {
+      status = 'error'
+      result = error
+    },
+  )
+
+  return {
+    read() {
+      if (status === 'pending') {
+        throw suspender
+      }
+      if (status === 'error') {
+        throw result
+      }
+      if (status === 'success') {
+        return result
+      }
+    },
+  }
+}
+
+let pokemon = suspenseify(
+  fetch('https://pokeapi.co/api/v2/pokemon/2').then(res => res.json()),
+)
+
 export default function PokemonDetail() {
-  return <div>Static Pokemon!</div>
+  return <div>{pokemon.read().name}</div>
 }
