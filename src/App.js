a843f1fc3494727d1fb9b31653426d0efa5330ee
yyh123 <yunhaoyu@gmail.com>
2020-01-15 15:10:44 +0800

    feat: add React.deferredValue

--- src/App.js
+++ src/App.js
@@ -1,15 +1,37 @@
-import React, { lazy, Suspense } from 'react'
+import React, { lazy, Suspense, useState } from 'react'
 import ErrorBoundary from './ErrorBoundary'
-// import PokemonDetail from './pokemon-detail'
-// create an import error by giving React.lazy a promise.reject()
+import { fetchPokemon, suspenseify } from './api'
 const PokemonDetail = lazy(() => import('./pokemon-detail'))
 
+let initialPokemon = suspenseify(fetchPokemon(1))
+
 export default function App() {
+  let [pokemon, setPokemon] = useState(initialPokemon)
+  let deferredPokemon = React.useDeferredValue(pokemon, { timeoutMs: 3000 })
+  let deferredPokemonIsStale = deferredPokemon !== pokemon
+  let [startTransition] = React.useTransition()
   return (
     <div>
+      <h1>Pokedex</h1>
       <ErrorBoundary fallback="Something is Wrong!">
         <Suspense fallback={'Loading your Pokemon...'}>
-          <PokemonDetail />
+          <PokemonDetail
+            resource={deferredPokemon}
+            isStale={deferredPokemonIsStale}
+          />
+          <button
+            type="button"
+            disabled={deferredPokemonIsStale}
+            onClick={() =>
+              startTransition(() =>
+                setPokemon(
+                  suspenseify(fetchPokemon(deferredPokemon.read().id + 1)),
+                ),
+              )
+            }
+          >
+            下一个
+          </button>
         </Suspense>
       </ErrorBoundary>
     </div>
--- src/pokemon-detail.js
+++ src/pokemon-detail.js
@@ -1,27 +1,14 @@
 import React, { useState } from 'react'
-import { fetchPokemon, suspenseify } from './api'
 import { DelaySpinner } from './ui'
-let initialPokemon = suspenseify(fetchPokemon(2))
 
-export default function PokemonDetail() {
-  let [pokemonResource, setPokemonResource] = useState(initialPokemon)
-  let [startTransition, isPending] = React.useTransition({ timeoutMs: 1000 })
-  let pokemon = pokemonResource.read()
+export default function PokemonDetail({ resource, isStale }) {
+  let pokeman = resource.read()
   return (
     <div>
-      <div>
-        {pokemon.name} {isPending && <DelaySpinner />}
+      <div style={isStale ? { opacity: 0.5 } : null}>
+        {pokeman.name}
+        {isStale && <DelaySpinner />}
       </div>
-      <button
-        disabled={isPending}
-        onClick={() =>
-          startTransition(() =>
-            setPokemonResource(suspenseify(fetchPokemon(pokemon.id + 1))),
-          )
-        }
-      >
-        下一个
-      </button>
     </div>
   )
 }
